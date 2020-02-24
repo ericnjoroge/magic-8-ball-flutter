dcd8f7c1a3b9f782b8dc9cb017e6989dd80bfbd1
Erick Njoroge <erick.njoroge@betika.com>
2020-02-24 18:25:01 +0300

    created stateless widget for app scaffold and stateful widget for body

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.blue[800],
+          backgroundColor: Colors.blue[200],
           appBar: AppBar(
             title: Text('Ask Me Anything'),
-            backgroundColor: Colors.blue,
+            backgroundColor: Colors.blue[800],
           ),
           body: BallPage(),
         ),
@@ -16,6 +16,31 @@ void main() => runApp(
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('I got pressed');
+              },
+              child: Image.asset('images/ball1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
